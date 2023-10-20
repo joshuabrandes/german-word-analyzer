89c3a0573da88502b682cd7fc75260a0b26dddd9
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2023-10-20 16:16:12 +0200

    I'm totally adding this to epic win. +300

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,46 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val dictionary = initDictionary()
+    while (true) {
+        println("Please type a letter (case sensitive) or exit to quit:")
+        var letter = readLine()!!
+        if (letter == "exit") {
+            break
+        } else if (letter.length > 1) {
+            println("Please type only one letter")
+            continue
+        } else
+            println("Please type a number (position):")
+        var position = readLine()!!.toInt()
+
+        var index = position - 1
+        val filteredDictionary: List<String> =
+            if (position == 0) {
+                dictionary.filter { it.startsWith(letter, false) }
+            } else {
+                dictionary.filter {
+                    return@filter if (it.length < position)
+                        false
+                    else
+                        it[index].toString() == (letter)
+                }
+            }
+        println(
+            "There are ${filteredDictionary.size} words starting with $letter " +
+                    "at position $position \n\n"
+        )
+        println("Do you want to see the words? (y/n)")
+        val answer = readLine()!!
+        if (answer == "y") {
+            filteredDictionary.forEach { println(it) }
+        }
+    }
+}
+
+fun initDictionary(): List<String> {
+    val url = ClassLoader.getSystemResource("german/german.dic")
+    val file = File(url.toURI())
+    return file.readLines(Charsets.ISO_8859_1)
+        .map { it.trim() }
 }
